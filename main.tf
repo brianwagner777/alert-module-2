2975635f6811e8ebf8b416ba73352855f93980f2
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 14:30:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# Create scheduled query alerts the Action Groups
-resource "azurerm_monitor_action_group" "action_group" {
-  count               = length(var.scheduled_query_alerts)
+# Create actions groups for query alerts
+resource "azurerm_monitor_action_group" "query_action_group" {
+  count               = length(var.query_alerts)
   resource_group_name = data.azurerm_resource_group.main.name
-  name                = var.scheduled_query_alerts[count.index].action_group_name
-  short_name          = substr(var.scheduled_query_alerts[count.index].action_group_short_name, 0, 12)
+  name                = var.query_alerts[count.index].action_group_name
+  short_name          = substr(var.query_alerts[count.index].action_group_short_name, 0, 12)
   tags                = var.tags
 
   dynamic "email_receiver" {
-    for_each = var.scheduled_query_alerts[count.index].action_group_email_receivers
+    for_each = var.query_alerts[count.index].action_group_email_receivers
     content {
       name                    = email_receiver.value["name"]
       email_address           = email_receiver.value["email_address"]
@@ -16,25 +16,26 @@ resource "azurerm_monitor_action_group" "action_group" {
   }
 }
 
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alert_rule" {
-  count               = length(var.scheduled_query_alerts)
-  name                = var.scheduled_query_alerts[count.index].alert_name
+# Create rules for query alerts
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "query_alert_rule" {
+  count               = length(var.query_alerts)
+  name                = var.query_alerts[count.index].alert_name
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  description         = var.scheduled_query_alerts[count.index].alert_description
-  enabled             = var.scheduled_query_alerts[count.index].alert_enabled
-  display_name        = var.scheduled_query_alerts[count.index].alert_name
+  description         = var.query_alerts[count.index].alert_description
+  enabled             = var.query_alerts[count.index].alert_enabled
+  display_name        = var.query_alerts[count.index].alert_name
   tags                = var.tags
 
-  evaluation_frequency = var.scheduled_query_alerts[count.index].alert_evaluation_frequency
-  window_duration      = var.scheduled_query_alerts[count.index].alert_window_duration
-  scopes               = [var.scheduled_query_alerts[count.index].alert_scope_resource_id]
-  severity             = var.scheduled_query_alerts[count.index].alert_severity
+  evaluation_frequency = var.query_alerts[count.index].alert_evaluation_frequency
+  window_duration      = var.query_alerts[count.index].alert_window_duration
+  scopes               = [var.query_alerts[count.index].alert_scope_resource_id]
+  severity             = var.query_alerts[count.index].alert_severity
   criteria {
-    query                   = var.scheduled_query_alerts[count.index].alert_criteria_query
-    time_aggregation_method = var.scheduled_query_alerts[count.index].alert_criteria_time_aggregation_method
-    threshold               = var.scheduled_query_alerts[count.index].alert_criteria_threshold
-    operator                = var.scheduled_query_alerts[count.index].alert_criteria_operator
+    query                   = var.query_alerts[count.index].alert_criteria_query
+    time_aggregation_method = var.query_alerts[count.index].alert_criteria_time_aggregation_method
+    threshold               = var.query_alerts[count.index].alert_criteria_threshold
+    operator                = var.query_alerts[count.index].alert_criteria_operator
     failing_periods {
       minimum_failing_periods_to_trigger_alert = 1
       number_of_evaluation_periods             = 1
@@ -45,7 +46,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alert_rule" {
   workspace_alerts_storage_enabled = false
   skip_query_validation            = true
   action {
-    action_groups = [azurerm_monitor_action_group.action_group[count.index].id]
+    action_groups = [azurerm_monitor_action_group.query_action_group[count.index].id]
   }
 
   lifecycle {
