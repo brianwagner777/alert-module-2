05cafdf63fe3ed641f53a8282360dc98eeb65e95
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 13:11:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "azurerm_monitor_action_group" "action_group" {
   count               = length(var.scheduled_query_alerts)
   name                = var.scheduled_query_alerts[count.index].action_group_name
   resource_group_name = data.azurerm_resource_group.main.name
-  short_name          = var.scheduled_query_alerts[count.index].action_group_short_name
+  short_name          = substr(var.scheduled_query_alerts[count.index].action_group_short_name, 0, 12)
   tags                = var.tags
 }
