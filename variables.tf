a728716f3e284e67696d2e2e9fb24f5a791ead7a
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 12:26:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the resource group where the resources will be created."
+  type        = string
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all created resources."
+  default     = {}
+}
+
 variable "scheduled_query_alerts" {
   type = list(object({ 
     action_group_name                      = string, 
