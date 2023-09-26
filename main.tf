170412c9cf0f12aaa1bce0414abdbe148fd7560c
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 13:24:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create scheduled query alerts the Action Groups
 resource "azurerm_monitor_action_group" "action_group" {
   count               = length(var.scheduled_query_alerts)
-  name                = var.scheduled_query_alerts[count.index].action_group_name
   resource_group_name = data.azurerm_resource_group.main.name
+  name                = var.scheduled_query_alerts[count.index].action_group_name
   short_name          = substr(var.scheduled_query_alerts[count.index].action_group_short_name, 0, 12)
   tags                = var.tags
 
@@ -13,5 +13,28 @@ resource "azurerm_monitor_action_group" "action_group" {
       email_address           = email_receiver.value["email_address"]
       use_common_alert_schema = true
     }
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alert_rule" {
+  count               = length(var.scheduled_query_alerts)
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  description         = var.scheduled_query_alerts[count.index].alert_description
+  enabled             = var.scheduled_query_alerts[count.index].alert_enabled
+  display_name        = var.scheduled_query_alerts[count.index].alert_name
+  tags                = var.tags
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  skip_query_validation            = true
+  action {
+    action_groups = [azurerm_monitor_action_group.action_group[count.index].id]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      enabled
+    ]
   }
 }
