273d0bcbcf011d34c8a3646c80b2ccbd14da7a5e
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 14:46:42 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,19 +11,42 @@ variable "tags" {
 
 variable "query_alerts" {
   type = list(object({ 
-    action_group_name                      = string, 
-    action_group_short_name                = string,
-    action_group_email_receivers           = list(object({ name = string, email_address = string })),
-    alert_name                             = string,
-    alert_description                      = string,
-    alert_enabled                          = bool,
-    alert_evaluation_frequency             = string,
-    alert_window_duration                  = string,
-    alert_severity                         = number,
-    alert_scope_resource_id                = string,
-    alert_criteria_query                   = string,
-    alert_criteria_time_aggregation_method = string,
-    alert_criteria_threshold               = number,
-    alert_criteria_operator                = string
-  }))
+      action_group_name                      = string, 
+      action_group_short_name                = string,
+      action_group_email_receivers           = list(object({ name = string, email_address = string })),
+      alert_name                             = string,
+      alert_description                      = string,
+      alert_enabled                          = bool,
+      alert_evaluation_frequency             = string,
+      alert_window_duration                  = string,
+      alert_severity                         = number,
+      alert_scope_resource_id                = string,
+      alert_criteria_query                   = string,
+      alert_criteria_time_aggregation_method = string,
+      alert_criteria_threshold               = number,
+      alert_criteria_operator                = string
+    })
+
+    default = {
+    }
+
+    description = <<EOT
+      query_alert = {
+        action_group_name: ""
+        action_group_short_name: ""
+        action_group_email_receivers: ""
+        alert_name: ""
+        alert_description: ""
+        alert_enabled: ""
+        alert_evaluation_frequency: ""
+        alert_window_duration: ""
+        alert_severity: ""
+        alert_scope_resource_id: ""
+        alert_criteria_query: ""
+        alert_criteria_time_aggregation_method: ""
+        alert_criteria_threshold: ""
+        alert_criteria_operator: ""
+      }
+    EOT
+  )
 }
