12f1bde115249bc1b4eabf3218229d35e28b2bea
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 13:09:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create scheduled query alerts the Action Groups
 resource "azurerm_monitor_action_group" "action_group" {
-  for_each            = var.scheduled_query_alerts
-  name                = each.value.action_group_name
+  count               = length(var.scheduled_query_alerts)
+  name                = var.scheduled_query_alerts[count.index].action_group_name
   resource_group_name = data.azurerm_resource_group.main.name
-  short_name          = each.value.action_group_short_name
+  short_name          = var.scheduled_query_alerts[count.index].action_group_short_name
   tags                = var.tags
 }
