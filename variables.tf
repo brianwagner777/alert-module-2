512dcd36e7dbd41de782fe77d91a3fffd38b02ba
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 13:14:07 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,7 +11,8 @@ variable "tags" {
 
 variable "scheduled_query_alerts" {
   type = list(object({ 
-    action_group_name       = string, 
-    action_group_short_name = string
+    action_group_name            = string, 
+    action_group_short_name      = string,
+    action_group_email_receivers = list(object({ name = string, email_address = string }))
   }))
 }
