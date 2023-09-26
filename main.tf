6698bd86fa9057942a9c594e4cee7be11c848799
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-26 13:13:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@ resource "azurerm_monitor_action_group" "action_group" {
   resource_group_name = data.azurerm_resource_group.main.name
   short_name          = substr(var.scheduled_query_alerts[count.index].action_group_short_name, 0, 12)
   tags                = var.tags
+
+  dynamic "email_receiver" {
+    for_each = var.scheduled_query_alerts[count.index].action_group_email_receivers
+    content {
+      name                    = email_receiver.value["name"]
+      email_address           = email_receiver.value["email_address"]
+      use_common_alert_schema = true
+    }
+  }
 }
