75f9d67a9ce48178a9b69315a4f19c759122e77b
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-27 15:31:55 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "query_alerts" {
   description = "Rules created for the query alerts."
-  value       = azurerm_monitor_scheduled_query_rules_alert_v2.query_alert_rule
+  value       = azurerm_monitor_scheduled_query_rules_alert_v2.query_alert_rules
 }
