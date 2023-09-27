9db508391ab4c3e0dd8c9d51f58fee7bc6edc877
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-27 10:46:55 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "query_alerts" {
   description = "Logic app with all attributes."
-  value       = azurerm_monitor_scheduled_query_rules_alert_v2.query_alert_rule
+  value       = "My test output"
 }
