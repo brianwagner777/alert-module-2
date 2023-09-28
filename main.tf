bdd2bcbf324ecd0a56aeb07bff55fc2935bbeb86
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-09-28 14:44:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@
 resource "azurerm_monitor_action_group" "query_alert_action_groups" {
   count               = length(var.query_alerts)
   resource_group_name = var.resource_group_name
-  name                = var.query_alerts[count.index].action_group_name
-  short_name          = substr(var.query_alerts[count.index].action_group_short_name, 0, 12)
+  name                = var.query_alerts[count.index].action_group.name
+  short_name          = substr(var.query_alerts[count.index].action_group.short_name, 0, 12)
   tags                = var.tags
 
   dynamic "email_receiver" {
-    for_each = var.query_alerts[count.index].action_group_email_receivers
+    for_each = var.query_alerts[count.index].action_group.email_receivers
     content {
       name                    = email_receiver.value["name"]
       email_address           = email_receiver.value["email_address"]
@@ -19,34 +19,41 @@ resource "azurerm_monitor_action_group" "query_alert_action_groups" {
 # Create query alerts rules
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "query_alert_rules" {
   count               = length(var.query_alerts)
-  name                = var.query_alerts[count.index].rule_name
+  name                = var.query_alerts[count.index].rule.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  description         = var.query_alerts[count.index].rule_description
-  enabled             = var.query_alerts[count.index].rule_enabled
-  display_name        = var.query_alerts[count.index].rule_name
+  description         = var.query_alerts[count.index].rule.description
+  enabled             = var.query_alerts[count.index].rule.enabled
+  display_name        = var.query_alerts[count.index].rule.name
   tags                = var.tags
 
-  evaluation_frequency = var.query_alerts[count.index].rule_evaluation_frequency
-  window_duration      = var.query_alerts[count.index].rule_window_duration
-  scopes               = var.query_alerts[count.index].rule_scope_resource_ids
-  severity             = var.query_alerts[count.index].rule_severity
-  criteria {
-    query                   = var.query_alerts[count.index].rule_criteria_query
-    time_aggregation_method = var.query_alerts[count.index].rule_criteria_time_aggregation_method
-    threshold               = var.query_alerts[count.index].rule_criteria_threshold
-    operator                = var.query_alerts[count.index].rule_criteria_operator
-    failing_periods {
-      minimum_failing_periods_to_trigger_alert = 1
-      number_of_evaluation_periods             = 1
-    }
-  }
+  evaluation_frequency              = var.query_alerts[count.index].rule.evaluation_frequency
+  scopes                            = var.query_alerts[count.index].rule.scopes
+  severity                          = var.query_alerts[count.index].rule.severity
+  window_duration                   = var.query_alerts[count.index].rule.window_duration
+  auto_mitigation_enabled           = var.query_alerts[count.index].rule.auto_mitigation_enabled
+  workspace_alerts_storage_enabled  = var.query_alerts[count.index].rule.workspace_alerts_storage_enabled
+  mute_actions_after_alert_duration = var.query_alerts[count.index].rule.mute_actions_after_alert_duration
+  query_time_range_override         = var.query_alerts[count.index].rule.query_time_range_override
+  skip_query_validation             = var.query_alerts[count.index].rule.skip_query_validation
+  target_resource_types             = var.query_alerts[count.index].rule.target_resource_types
 
-  auto_mitigation_enabled          = false
-  workspace_alerts_storage_enabled = false
-  skip_query_validation            = true
   action {
     action_groups = [azurerm_monitor_action_group.query_alert_action_groups[count.index].id]
+  }
+
+  criteria {
+    operator                = var.query_alerts[count.index].rule.criteria_operator
+    query                   = var.query_alerts[count.index].rule.criteria_query
+    threshold               = var.query_alerts[count.index].rule.criteria_threshold
+    time_aggregation_method = var.query_alerts[count.index].rule.criteria_time_aggregation_method
+    metric_measure_column   = var.query_alerts[count.index].rule.criteria_metric_measure_column
+    resource_id_column      = var.query_alerts[count.index].rule.criteria_resource_id_column
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = var.query_alerts[count.index].rule.criteria_minimum_failing_periods_to_trigger_alert
+      number_of_evaluation_periods             = var.query_alerts[count.index].rule.criteria_number_of_evaluation_periods
+    }
   }
 
   lifecycle {
