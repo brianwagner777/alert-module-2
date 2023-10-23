406df64ff08d7ec201bc3bc244a56c220f9f92c2
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-10-23 12:13:19 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "query_alerts" {
-  description = "Rules created for the query alerts."
+output "action_groups" {
+  description = "Action groups with all attributes."
+  value       = azurerm_monitor_action_group.action_groups
+}
+
+output "query_alert_rules" {
+  description = "Scheduled query (log) alert rules with all attributes."
   value       = azurerm_monitor_scheduled_query_rules_alert_v2.query_alert_rules
+}
+
+output "metric_alert_rules" {
+  description = "Metric alert rules with all attributes."
+  value       = azurerm_monitor_metric_alert.metric_alert_rules
 }
