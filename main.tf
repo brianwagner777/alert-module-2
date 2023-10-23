6bcc550150a0e582a535aca3ce9729e6ab45992e
brianwagner777 <105369305+brianwagner777@users.noreply.github.com>
2023-10-23 15:16:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,155 +1,77 @@
-# Create actions groups
-# https://stackoverflow.com/questions/58594506/how-to-for-each-through-a-listobjects-in-terraform-0-12
-resource "azurerm_monitor_action_group" "action_groups" {
-  for_each            = { for idx, ag in var.action_groups : ag.name => ag }
-  resource_group_name = var.resource_group_name
-  name                = each.value.name
-  short_name          = substr(each.value.short_name, 0, 12)
-  tags                = var.tags
-
-  dynamic "email_receiver" {
-    for_each = each.value.email_receivers
-    content {
-      name                    = email_receiver.value["name"]
-      email_address           = email_receiver.value["email_address"]
-      use_common_alert_schema = true
-    }
+# Email receivers
+locals {
+  email_receiver_bwagner77 = {
+    name          = "sendto-bwagner"
+    email_address = "bwagner77@live.com"
+  }
+  email_receiver_brianwagner777 = {
+    name          = "sendto-brianwagner777"
+    email_address = "brian.wagner777@outlook.com"
   }
 }
 
-# Create query (log) alerts rules
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "query_alert_rules" {
-  for_each            = { for idx, qar in var.query_alert_rules : qar.name => qar }
-  name                = each.value.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  description         = each.value.description
-  enabled             = each.value.enabled
-  display_name        = each.value.name
-  tags                = var.tags
+# Create alerts
+module "alerts" {
+  source = "git::https://github.com/brianwagner777/alert-module-2.git"
 
-  evaluation_frequency              = each.value.evaluation_frequency
-  scopes                            = [each.value.scope]
-  severity                          = each.value.severity
-  window_duration                   = each.value.window_duration
-  auto_mitigation_enabled           = each.value.auto_mitigation_enabled
-  workspace_alerts_storage_enabled  = each.value.workspace_alerts_storage_enabled
-  mute_actions_after_alert_duration = each.value.mute_actions_after_alert_duration
-  query_time_range_override         = each.value.query_time_range_override
-  skip_query_validation             = each.value.skip_query_validation
-  target_resource_types             = each.value.target_resource_types
+  resource_group_name = data.azurerm_resource_group.rg_shared.name
+  location            = data.azurerm_resource_group.rg_shared.location
+  tags                = local.tags
 
-  # https://build5nines.com/terraform-expression-get-list-object-by-attribute-value-lookup/
-  # https://dev.to/pwd9000/terraform-filter-results-using-for-loops-4n75
+  action_groups = [
+    {
+      name            = "IntPocLoggingFailure"
+      short_name      = "IntPocLogFail"
+      email_receivers = [local.email_receiver_bwagner77, local.email_receiver_brianwagner777]
+    }
+  ]
 
-  action {
-    action_groups     = toset([for ag in azurerm_monitor_action_group.action_groups : ag.id if contains(each.value.action_group_names, ag.name)])
-    custom_properties = each.value.action_custom_properties
-  }
+  query_alert_rules = [
+    {
+      name                 = "sqr-logs-shared-${var.environment}"
+      description          = "Alert when log count does not mee threshold"
+      enabled              = false
+      evaluation_frequency = "P1D"
+      scope                = data.azurerm_log_analytics_workspace.log_shared.id
+      severity             = 1
+      window_duration      = "P1D"
+      action_group_names   = ["IntPocLoggingFailure"]
+      action_custom_properties = {
+        "key1" = "test1"
+        "key2" = "test2"
+      }
 
-  criteria {
-    operator                = each.value.criteria.operator
-    query                   = each.value.criteria.query
-    threshold               = each.value.criteria.threshold
-    time_aggregation_method = each.value.criteria.time_aggregation_method
-    metric_measure_column   = each.value.criteria.metric_measure_column
-    resource_id_column      = each.value.criteria.resource_id_column
-
-    dynamic "dimension" {
-      for_each = each.value.criteria.dimension[*]
-      content {
-        name     = each.value.criteria.dimension.name
-        operator = each.value.criteria.dimension.operator
-        values   = each.value.criteria.dimension.values
+      criteria = {
+        operator                = "Equal"
+        threshold               = 0
+        time_aggregation_method = "Count"
+        query                   = <<-QUERY
+          FunctionAppLogs
+          | where _ResourceId has "${data.azurerm_resource_group.rg_shared.name}" 
+              and AppName == "TODO"
+        QUERY
       }
     }
+  ]
 
-    dynamic "failing_periods" {
-      for_each = each.value.criteria.failing_periods[*]
-      content {
-        minimum_failing_periods_to_trigger_alert = each.value.criteria.failing_periods.minimum_failing_periods_to_trigger_alert
-        number_of_evaluation_periods             = each.value.criteria.failing_periods.number_of_evaluation_periods
+  metric_alert_rules = [
+    {
+      name              = "sqr-metrics-shared-${var.environment}"
+      description       = "Metric alert"
+      enabled           = false
+      frequency         = "PT1H"
+      scopes            = [data.azurerm_log_analytics_workspace.log_shared.id]
+      severity          = 1
+      window_size       = "PT1H"
+      action_group_name = "IntPocLoggingFailure"
+
+      criteria = {
+        metric_namespace = "Microsoft.Storage/storageAccounts"
+        metric_name      = "Transactions"
+        aggregation      = "Total"
+        operator         = "GreaterThan"
+        threshold        = 5000
       }
     }
-  }
-
-  lifecycle {
-    ignore_changes = [
-      enabled
-    ]
-  }
-}
-
-# Create metric alert rules
-resource "azurerm_monitor_metric_alert" "metric_alert_rules" {
-  for_each            = { for idx, mar in var.metric_alert_rules : mar.name => mar }
-  name                = each.value.name
-  resource_group_name = var.resource_group_name
-  description         = each.value.description
-  enabled             = each.value.enabled
-  tags                = var.tags
-
-  scopes                   = each.value.scopes
-  auto_mitigate            = each.value.auto_mitigate
-  frequency                = each.value.frequency
-  severity                 = each.value.severity
-  target_resource_type     = each.value.target_resource_type
-  target_resource_location = each.value.target_resource_location
-  window_size              = each.value.window_size
-
-  action {
-    action_group_id    = one([for ag in azurerm_monitor_action_group.action_groups : ag.id if each.value.action_group_name == ag.name])
-    webhook_properties = each.value.action_webhook_properties
-  }
-
-  dynamic "criteria" {
-    for_each = each.value.criteria[*]
-    content {
-      metric_namespace       = each.value.criteria.metric_namespace
-      metric_name            = each.value.criteria.metric_name
-      aggregation            = each.value.criteria.aggregation
-      operator               = each.value.criteria.operator
-      threshold              = each.value.criteria.threshold
-      skip_metric_validation = each.value.criteria.skip_metric_validation
-
-      dynamic "dimension" {
-        for_each = each.value.criteria.dimension[*]
-        content {
-          name     = each.value.criteria.dimension.name
-          operator = each.value.criteria.dimension.operator
-          values   = each.value.criteria.dimension.values
-        }
-      }
-    }
-  }
-
-  dynamic "dynamic_criteria" {
-    for_each = each.value.dynamic_criteria[*]
-    content {
-      metric_namespace         = each.value.dynamic_criteria.metric_namespace
-      metric_name              = each.value.dynamic_criteria.metric_name
-      aggregation              = each.value.dynamic_criteria.aggregation
-      operator                 = each.value.dynamic_criteria.operator
-      alert_sensitivity        = each.value.dynamic_criteria.alert_sensitivity
-      evaluation_total_count   = each.value.dynamic_criteria.evaluation_total_count
-      evaluation_failure_count = each.value.dynamic_criteria.evaluation_failure_count
-      ignore_data_before       = each.value.dynamic_criteria.ignore_data_before
-      skip_metric_validation   = each.value.dynamic_criteria.skip_metric_validation
-
-      dynamic "dimension" {
-        for_each = each.value.dynamic_criteria.dimension[*]
-        content {
-          name     = each.value.dynamic_criteria.dimension.name
-          operator = each.value.dynamic_criteria.dimension.operator
-          values   = each.value.dynamic_criteria.dimension.values
-        }
-      }
-    }
-  }
-
-  lifecycle {
-    ignore_changes = [
-      enabled
-    ]
-  }
+  ]
 }
